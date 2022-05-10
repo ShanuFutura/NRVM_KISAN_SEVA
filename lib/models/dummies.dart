35ca9bda0c48697ebdd5d836febbfd67fb6c3833
Shanu Futura <shanu.futura@gmail.com>
2022-05-10 15:08:28 +0530

    implemented machine learning

--- lib/models/dummies.dart
+++ lib/models/dummies.dart
@@ -1,6 +1,6 @@
 class Dummies {
-  static const String rootUrl = 'http://192.168.68.127/farmer/API/';
-  static const String rootUrlforImages = 'http://192.168.68.127/farmer/';
+  static const String rootUrl = 'http://192.168.68.121/farmer/API/';
+  static const String rootUrlforImages = 'http://192.168.68.121/farmer/';
   static const String iotDataUrl =
       'https://api.thingspeak.com/channels/1682250/feeds.json?api_key=YKEC2RJZ64W8IPS0%20&results=2';
 }
