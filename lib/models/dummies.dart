1bb79dead90288ba64282f1f03a6a507bdc6b81a
Shanu Futura <shanu.futura@gmail.com>
2022-05-31 10:30:51 +0530

    last

--- lib/models/dummies.dart
+++ lib/models/dummies.dart
@@ -2,5 +2,5 @@ class Dummies {
   static const String rootUrl = 'http://192.168.68.133/farmer/API/';
   static const String rootUrlforImages = 'http://192.168.68.133/farmer/';
   static const String iotDataUrl =
-      'https://api.thingspeak.com/channels/1682250/feeds.json?api_key=YKEC2RJZ64W8IPS0%20&results=2';
+      'https://api.thingspeak.com/channels/1753489/feeds.json?api_key=RZ4DI81NMX6FCOQ3&results=2';
 }
